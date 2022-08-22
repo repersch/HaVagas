6c22bdf3b8d1c8d5d5fd54b12f8d68fbd8782472
Renata Persch <renatapersch@hotmail.com>
2022-08-22 15:15:18 -0300

    Formulário finalizado

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/Cadastro.java
@@ -0,0 +1,21 @@
+package br.edu.ifsp.scl.ads.pdm.havagas;
+
+import java.time.LocalDate;
+
+public class Cadastro {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String tipoTelefone;
+    private String celular;
+    private String sexo;
+    private LocalDate dataNascimento;
+    private String formacao;
+    private Integer anoConclusao;
+    private String instituicao;
+    private String tituloMonografia;
+    private String orientador;
+    private String vagasDeInteresse;
+
+}
--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/MainActivity.java
@@ -3,12 +3,57 @@ package br.edu.ifsp.scl.ads.pdm.havagas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import br.edu.ifsp.scl.ads.pdm.havagas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding amb;
+    private Cadastro adastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(amb.getRoot());
+
+        amb.limparBt.setOnClickListener( view -> {
+            amb.nomeEt.setText("");
+            amb.emailEt.setText("");
+            amb.receberEmailCb.setChecked(false);
+            amb.telefoneEt.setText("");
+            amb.celularEt.setText("");
+            amb.femininoRb.setChecked(true);
+            amb.dataNascimentoEt.setText("");
+            amb.formacaoSp.setSelection(0);
+            amb.vagasDeInteresseEt.setText("");
+        });
+
+
+        amb.formacaoSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicao, long l) {
+                if (posicao == 1 || posicao == 2) {
+                    amb.fundamentalMedioLl.setVisibility(View.VISIBLE);
+                } else if (posicao == 3 || posicao == 4) {
+                    amb.graduacaoEspecializacaoLl.setVisibility(View.VISIBLE);
+                } else if (posicao == 5 || posicao == 6) {
+                    amb.mestradoDoutoradoLl.setVisibility(View.VISIBLE);
+                } else {
+                    amb.fundamentalMedioLl.setVisibility(View.GONE);
+                    amb.graduacaoEspecializacaoLl.setVisibility(View.GONE);
+                    amb.mestradoDoutoradoLl.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
