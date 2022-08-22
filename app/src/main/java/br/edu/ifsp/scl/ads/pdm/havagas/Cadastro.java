25bda56e23a112af3520eb0f8abf6b37ba4e00a8
Renata Persch <renatapersch@hotmail.com>
2022-08-22 19:15:35 -0300

    Recuperando dados

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/Cadastro.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/Cadastro.java
@@ -6,11 +6,12 @@ public class Cadastro {
 
     private String nome;
     private String email;
+    private Boolean receberEmail;
     private String telefone;
     private String tipoTelefone;
     private String celular;
     private String sexo;
-    private LocalDate dataNascimento;
+    private String dataNascimento;
     private String formacao;
     private Integer anoConclusao;
     private String instituicao;
@@ -18,4 +19,155 @@ public class Cadastro {
     private String orientador;
     private String vagasDeInteresse;
 
+    public Cadastro() {
+    }
+
+    public Cadastro(String nome, String email, Boolean receberEmail, String telefone, String tipoTelefone, String celular, String sexo, String dataNascimento, String formacao, Integer anoConclusao, String instituicao, String tituloMonografia, String orientador, String vagasDeInteresse) {
+        this.nome = nome;
+        this.email = email;
+        this.receberEmail = receberEmail;
+        this.telefone = telefone;
+        this.tipoTelefone = tipoTelefone;
+        this.celular = celular;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.formacao = formacao;
+        this.anoConclusao = anoConclusao;
+        this.instituicao = instituicao;
+        this.tituloMonografia = tituloMonografia;
+        this.orientador = orientador;
+        this.vagasDeInteresse = vagasDeInteresse;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getReceberEmail() {
+        return receberEmail;
+    }
+
+    public void setReceberEmail(Boolean receberEmail) {
+        this.receberEmail = receberEmail;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipoTelefone() {
+        return tipoTelefone;
+    }
+
+    public void setTipoTelefone(String tipoTelefone) {
+        this.tipoTelefone = tipoTelefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public Integer getAnoConclusao() {
+        return anoConclusao;
+    }
+
+    public void setAnoConclusao(Integer anoConclusao) {
+        this.anoConclusao = anoConclusao;
+    }
+
+    public String getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(String instituicao) {
+        this.instituicao = instituicao;
+    }
+
+    public String getTituloMonografia() {
+        return tituloMonografia;
+    }
+
+    public void setTituloMonografia(String tituloMonografia) {
+        this.tituloMonografia = tituloMonografia;
+    }
+
+    public String getOrientador() {
+        return orientador;
+    }
+
+    public void setOrientador(String orientador) {
+        this.orientador = orientador;
+    }
+
+    public String getVagasDeInteresse() {
+        return vagasDeInteresse;
+    }
+
+    public void setVagasDeInteresse(String vagasDeInteresse) {
+        this.vagasDeInteresse = vagasDeInteresse;
+    }
+
+    @Override
+    public String toString() {
+        return "Cadastro{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", receberEmail=" + receberEmail +
+                ", telefone='" + telefone + '\'' +
+                ", tipoTelefone='" + tipoTelefone + '\'' +
+                ", celular='" + celular + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", formacao='" + formacao + '\'' +
+                ", anoConclusao=" + anoConclusao +
+                ", instituicao='" + instituicao + '\'' +
+                ", tituloMonografia='" + tituloMonografia + '\'' +
+                ", orientador='" + orientador + '\'' +
+                ", vagasDeInteresse='" + vagasDeInteresse + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/havagas/MainActivity.java
@@ -1,18 +1,23 @@
 package br.edu.ifsp.scl.ads.pdm.havagas;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+
+import java.time.LocalDate;
 
 import br.edu.ifsp.scl.ads.pdm.havagas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding amb;
-    private Cadastro adastro;
+    private Cadastro cadastro;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,32 +25,41 @@ public class MainActivity extends AppCompatActivity {
         amb = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(amb.getRoot());
 
-        amb.limparBt.setOnClickListener( view -> {
-            amb.nomeEt.setText("");
-            amb.emailEt.setText("");
-            amb.receberEmailCb.setChecked(false);
-            amb.telefoneEt.setText("");
-            amb.celularEt.setText("");
-            amb.femininoRb.setChecked(true);
-            amb.dataNascimentoEt.setText("");
-            amb.formacaoSp.setSelection(0);
-            amb.vagasDeInteresseEt.setText("");
-        });
+        amb.limparBt.setOnClickListener(this::limpar);
 
+        amb.salvarBt.setOnClickListener(this::salvar);
+
+        amb.cadastrarCelularSw.setOnClickListener(view -> {
+            if (amb.cadastrarCelularSw.isChecked()) {
+                amb.celularEt.setVisibility(View.VISIBLE);
+            } else {
+                amb.celularEt.setVisibility(View.GONE);
+            }
+        });
 
         amb.formacaoSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int posicao, long l) {
                 if (posicao == 1 || posicao == 2) {
-                    amb.fundamentalMedioLl.setVisibility(View.VISIBLE);
+                    amb.anoConclusaoEt.setVisibility(View.VISIBLE);
+                    amb.instituicaoEt.setVisibility(View.GONE);
+                    amb.tituloMonografiaEt.setVisibility(View.GONE);
+                    amb.orientadorEt.setVisibility(View.GONE);
                 } else if (posicao == 3 || posicao == 4) {
-                    amb.graduacaoEspecializacaoLl.setVisibility(View.VISIBLE);
+                    amb.anoConclusaoEt.setVisibility(View.VISIBLE);
+                    amb.instituicaoEt.setVisibility(View.VISIBLE);
+                    amb.tituloMonografiaEt.setVisibility(View.GONE);
+                    amb.orientadorEt.setVisibility(View.GONE);
                 } else if (posicao == 5 || posicao == 6) {
-                    amb.mestradoDoutoradoLl.setVisibility(View.VISIBLE);
+                    amb.anoConclusaoEt.setVisibility(View.VISIBLE);
+                    amb.instituicaoEt.setVisibility(View.VISIBLE);
+                    amb.tituloMonografiaEt.setVisibility(View.VISIBLE);
+                    amb.orientadorEt.setVisibility(View.VISIBLE);
                 } else {
-                    amb.fundamentalMedioLl.setVisibility(View.GONE);
-                    amb.graduacaoEspecializacaoLl.setVisibility(View.GONE);
-                    amb.mestradoDoutoradoLl.setVisibility(View.GONE);
+                    amb.anoConclusaoEt.setVisibility(View.GONE);
+                    amb.instituicaoEt.setVisibility(View.GONE);
+                    amb.tituloMonografiaEt.setVisibility(View.GONE);
+                    amb.orientadorEt.setVisibility(View.GONE);
                 }
             }
 
@@ -55,5 +69,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void limpar(View view) {
+        amb.nomeEt.setText("");
+        amb.emailEt.setText("");
+        amb.receberEmailCb.setChecked(false);
+        amb.telefoneEt.setText("");
+        amb.celularEt.setText("");
+        amb.femininoRb.setChecked(true);
+        amb.dataNascimentoEt.setText("");
+        amb.formacaoSp.setSelection(0);
+        amb.vagasDeInteresseEt.setText("");
+        amb.cadastrarCelularSw.setChecked(false);
+        amb.celularEt.setVisibility(View.GONE);
+    }
+
+    private void salvar(View view) {
+        cadastro = new Cadastro();
+        cadastro.setNome(amb.nomeEt.getText().toString());
+        cadastro.setEmail(amb.emailEt.getText().toString());
+        cadastro.setReceberEmail(amb.receberEmailCb.isChecked());
+        cadastro.setTelefone(amb.telefoneEt.getText().toString());
+        if (amb.cadastrarCelularSw.isChecked()) {
+            cadastro.setCelular(amb.celularEt.getText().toString());
+        } else {
+            cadastro.setCelular(null);
+        }
+        cadastro.setSexo(amb.femininoRb.isChecked() ? "Feminino" : "Masculino");
+        cadastro.setDataNascimento(amb.dataNascimentoEt.getText().toString());
+        cadastro.setFormacao(amb.formacaoSp.getSelectedItem().toString());
+        String formacao = amb.formacaoSp.getSelectedItem().toString();
+        cadastro.setAnoConclusao(Integer.valueOf(amb.anoConclusaoEt.getText().toString()));
+        if (!formacao.equals("Fundamental") || !formacao.equals("Médio")) {
+            cadastro.setInstituicao(amb.instituicaoEt.getText().toString());
+        }
+
+        if (formacao.equals("Mestrado") || formacao.equals("Doutorado")) {
+            cadastro.setTituloMonografia(amb.tituloMonografiaEt.getText().toString());
+            cadastro.setOrientador(amb.orientadorEt.getText().toString());
+        }
+        cadastro.setVagasDeInteresse(amb.vagasDeInteresseEt.getText().toString());
+
+        System.out.println(cadastro);
+        limpar(view);
     }
 }
